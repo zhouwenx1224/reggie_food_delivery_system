983734a24f41bbe7a57e5bf7eb8b6f28eb4de701
zwx <wz2592@columbia.edu>
2023-02-28 14:10:48 -0500

    "Day1-Employee Functionalities"

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -10,6 +10,5 @@ public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
         log.info("Application Start Successfully! :)");
-        log.info("Application Start Successfully! :)");
     }
 }
